a76844069a1f95261e2cc725434167043dfd5c5b
letipb <lety_15pb@gmail.com>
2017-02-03 09:22:42 -0600

    Modificaciones2

--- Ejemplo1/src/com/ejemplo/textos/Funciones.java
+++ Ejemplo1/src/com/ejemplo/textos/Funciones.java
@@ -3,14 +3,31 @@ package com.ejemplo.textos;
 public class Funciones {
 
 	public Funciones(String cad1, int longi) {
-		this.cadena1=cad1;
-		this.longitud=longi;
+		this.setCadena1(cad1);
+		this.setLongitud(longi);
 	}
 	
 	public int tamaño(){
-		return(longitud=cadena1.length());
+		return(setLongitud(getCadena1().length()));
 	}
 		
+	private int getLongitud() {
+		return longitud;
+	}
+
+	private int setLongitud(int longitud) {
+		this.longitud = longitud;
+		return longitud;
+	}
+
+	private String getCadena1() {
+		return cadena1;
+	}
+
+	private void setCadena1(String cadena1) {
+		this.cadena1 = cadena1;
+	}
+
 	private String cadena1;
 	private int longitud;
 }
